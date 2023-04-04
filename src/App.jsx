bfbf2e5aef264d4e4256d20707203b1ac965cf6a
Carlos Franco Castanio <carlosfranco041797@gmail.com>
2023-04-04 17:23:21 -0500

    versionthree

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { useState } from "react";
 import axios from "axios";
 import Weather from "./components/Weather";
+import Loader from "./components/Loader";
 
 function App() {
   const [coords, setCoords] = useState();
-  const [weather, setWeather] = useState()
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
 
   const success = (pos) => {
     const currentCoords = {
@@ -14,6 +16,7 @@ function App() {
       lon: pos.coords.longitude,
     };
     setCoords(currentCoords);
+    
   };
 
   useEffect(() => {
@@ -26,15 +29,30 @@ function App() {
 
       axios
         .get(URL)
-        .then((res) => setWeather(res.data))
+        .then((res) =>{
+          setWeather(res.data)
+          const celcius=(res.data.main.temp-273.15).toFixed(1)
+    const fahrenheit=(celcius*(9/5)+32).toFixed(1)
+    const newTemp={
+      
+      celcius,
+      fahrenheit
+    }
+    setTemp(newTemp);
+        } )
         .catch((err) => console.log(err));
     }
   }, [coords]);
 
-  return (
-    <div className="App">
+  
 
-     <Weather weather={weather}/> 
+  return (
+    <div className="App grid place-content-center item-center min-h-screen bg-[url('/image/bg-sun.jpg')] bg-cover px-2">
+
+    {weather? ( <Weather weather={weather} temp={temp}/> ):(
+    <Loader/>
+    )
+    }
      
     </div>
   );
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,58 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Loader from "./Loader";
+ 
 
-const Weather = ({ weather }) => {
+const Weather = ({ weather,temp }) => {
+  const [isCelcius, setIsCelcius] = useState(true)
   console.log(weather);
+
+  const changeUnitTenp=()=>{
+
+    setIsCelcius(!isCelcius)
+
+  }
   return (
-    <section>
-      {weather ? (
-        <h2>
-          {weather?.name},{weather?.sys.country}
-        </h2>
-      ) : (
-        <Loader />
-      )}
+    <section className="text-xl">
+    
+        <h2 className="text-center mb-4 font-bold text-2xl">{weather.name},{weather.sys.country}</h2>
+     
 
-      <article>
-        <h3>{weather?.weather[0].description}</h3>
+      <section className="grid gap-4">
 
-        <h2>{parseInt(weather?.main.temp)} °K</h2>
+      <article className="bg-slate-300/70 rounded-3xl grid grid-cols-2 justify-item-center item-center">
+        <h3 className="capitalize col-start-1 col-end-3">{weather.weather[0].description}</h3>
+
+        <h2 className='text-[45px] font-light'>{isCelcius ? `${temp.celcius} °C`:`${temp.fahrenheit} °F`}</h2>
 
         <div>
           <img
-            src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
             alt=""
           />
         </div>
       </article>
-      <section>
-        <article>
+      </section>
 
-          <div>
+      <section>
+        <article className="bg-slate-300/70 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch mt-2">
+
+          <div className="flex text-sm justify-center items-center"> 
             <div>
               <img src="/public/image/viento.png" alt="" />
             </div>
-            <h5>{weather?.wind.speed} m/s</h5>
+            <h5>{weather.wind.speed} m/s</h5>
           </div>
 
-          <div>
+          <div  className="flex text-sm justify-center items-center">
             <div>
               <img src="/public/image/gotas.png" alt="" />
             </div>
-            <h5>{weather?.wind.deg} %</h5>
+            <h5>{weather.wind.deg} %</h5>
           </div>
 
-          <div>
+          <div  className="flex text-sm justify-center items-center">
             <div>
               <img src="/public/image/arrow.png" alt="" />
             </div>
-            <h5>{weather?.wind.gust} hpa</h5>
+            <h5>{weather.wind.gust} hpa</h5>
           </div>
 
-        </article>
+        </article>  
       </section>
 
-      <button className="bg-blue-500 py-2 px-6 text-white font-bolod rounded-full hover:bg-blue-800 duration-200 " >Change C°/F°</button>
+      <button onClick={changeUnitTenp} className="bg-blue-500 py-2 px-6 text-white font-bolod rounded-full hover:bg-blue-800 duration-200 text-sm block mx-auto" >Change C°/F°</button>
     </section>
   );
 };
